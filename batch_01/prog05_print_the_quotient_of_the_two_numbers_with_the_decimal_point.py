45e8b4239d58c2400913cad237e9b4b4e4cdfb65
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:47:16 +0800

    Prog05: Ask User for 2 input numbers

--- batch_01/prog05_print_the_quotient_of_the_two_numbers_with_the_decimal_point.py
+++ batch_01/prog05_print_the_quotient_of_the_two_numbers_with_the_decimal_point.py
@@ -1,7 +1,10 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
 
 #Ask the user for 2 input numbers
- 
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
 #Get the quotient
- 
+
 #Print the quotient with decimal points
