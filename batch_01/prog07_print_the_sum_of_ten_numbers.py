eb037d09d458d54c2a995d634abc5749493cc755
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 18:24:11 +0800

    Prog07: Get the sum

--- batch_01/prog07_print_the_sum_of_ten_numbers.py
+++ batch_01/prog07_print_the_sum_of_ten_numbers.py
@@ -3,7 +3,8 @@
 #Ask the user for 10 input numbers
 for i in range(10):
     num = int(input(f"Enter a number {i+1}: "))
-    
-#Get the sum
+    sum = 0
+    #Get the sum
+    sum += num
 
 #Print the sum
