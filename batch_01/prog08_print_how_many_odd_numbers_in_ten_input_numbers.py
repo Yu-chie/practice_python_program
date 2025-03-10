3295253e2bb3945f0070e196e1ca3f2257e5583a
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 19:02:42 +0800

    Prog08: Initialize odd_numbers, Determine if number is odd

--- batch_01/prog08_print_how_many_odd_numbers_in_ten_input_numbers.py
+++ batch_01/prog08_print_how_many_odd_numbers_in_ten_input_numbers.py
@@ -1,9 +1,14 @@
 #Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
+#Initialize odd number
+odd_numbers = 0
+
 #Ask the user for 10 input numbers
 for i in range(10):
     num = int(input(f"Enter a number {i+1}: "))
-
-#Determine if odd or even
-
+    
+    #Determine if odd or even
+    if num % 2 != 0:
+        odd_numbers += 1
+        
 #Print the number of odd numbers
