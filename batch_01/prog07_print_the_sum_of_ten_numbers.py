d24ccffdd49efda961c55a62563a1d059b47f45c
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 18:05:24 +0800

    Prog07: Ask User for 10 input numbers

--- batch_01/prog07_print_the_sum_of_ten_numbers.py
+++ batch_01/prog07_print_the_sum_of_ten_numbers.py
@@ -1,7 +1,9 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 #Ask the user for 10 input numbers
-
+for i in range(10):
+    num = int(input(f"Enter a number {i+1}: "))
+    
 #Get the sum
 
 #Print the sum
