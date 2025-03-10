05f6c7eeb55f798784c13429b0c4be971d9611dd
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:18:00 +0800

    Prog02: Pseudocode Added

---
+++ batch_01/prog02_print_equal_when_the_numbers_are_the_same.py
@@ -0,0 +1,7 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+#Ask the user for 2 input numbers
+
+#Determine if the numbers are equal
+
+#Print "Equal" when the numbers are the same.
