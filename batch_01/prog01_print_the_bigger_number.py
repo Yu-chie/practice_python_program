12d820346792d608aa7698b78180ebeebd7c4d22
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:05:30 +0800

    Prog01: Ask User for 2 input numbers

--- batch_01/prog01_print_the_bigger_number.py
+++ batch_01/prog01_print_the_bigger_number.py
@@ -1,6 +1,9 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 #Ask the user for 2 input numbers
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #Determine the bigger number
 
