3af5ebde129dd07bfd2d1a831b8215b4b57bb0eb
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:53:52 +0800

    Prog06: Ask User for 2 input numbers

--- batch_01/prog06_print_the_result_when_the _number_is_raised_to_the_second_number.py
+++ batch_01/prog06_print_the_result_when_the _number_is_raised_to_the_second_number.py
@@ -1,6 +1,9 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
 #Ask the user for 2 input numbers
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #Raise the First number with the second
 
