b97424534a7ea45b7368cb00156b5f5b82a6fb3c
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 19:11:38 +0800

    Prog10: Pseudocode Added

---
+++ batch_01/prog10_print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero.py
@@ -0,0 +1,7 @@
+#Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+#Loop through numbers 0 to 100
+
+#Check if the number does not end in 0
+
+#Print all numbers that does not end in 0
