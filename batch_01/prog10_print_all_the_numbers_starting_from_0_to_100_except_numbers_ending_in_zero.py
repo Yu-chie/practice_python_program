acc7c09125430dae89b006397e0e5ac9ceec73ba
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 19:12:52 +0800

    Prog10: Loop numbers 0 to 100

--- batch_01/prog10_print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero.py
+++ batch_01/prog10_print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero.py
@@ -1,7 +1,8 @@
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 #Loop through numbers 0 to 100
+for num in range(0, 101):
 
-#Check if the number does not end in 0
+    #Check if the number does not end in 0
 
-#Print all numbers that does not end in 0
+    #Print all numbers that does not end in 0
