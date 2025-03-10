988428657d7e6b96e0417d5fd7e32e64dca50849
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:19:17 +0800

    Prog02: Ask User for 2 input numbers

--- batch_01/prog02_print_equal_when_the_numbers_are_the_same.py
+++ batch_01/prog02_print_equal_when_the_numbers_are_the_same.py
@@ -1,6 +1,9 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
 #Ask the user for 2 input numbers
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #Determine if the numbers are equal
 
