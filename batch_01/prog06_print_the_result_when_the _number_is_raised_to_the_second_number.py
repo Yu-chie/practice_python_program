47ac6901f74e464c25b3812a115b851e8a7b26d8
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:53:05 +0800

    Prog06: Pseudocode Added

---
+++ batch_01/prog06_print_the_result_when_the _number_is_raised_to_the_second_number.py
@@ -0,0 +1,7 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+#Ask the user for 2 input numbers
+
+#Raise the First number with the second
+
+#Print the result
