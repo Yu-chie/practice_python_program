f9244be8d893ea7e67e2310bfb602a5c9f0f1094
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:43:03 +0800

    Prog05: Pseudocode Added

---
+++ batch_01/prog05_print_the_quotient_of_the_two_numbers_with_the_decimal_point.py
@@ -0,0 +1,7 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+#Ask the user for 2 input numbers
+ 
+#Get the quotient
+ 
+#Print the quotient with decimal points
