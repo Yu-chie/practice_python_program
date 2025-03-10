c2441351f2a6bd8a9253fd5066c95367416ad752
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 19:00:18 +0800

    Prog08: Ask User for 10 input numbers

--- batch_01/prog08_print_how_many_odd_numbers_in_ten_input_numbers.py
+++ batch_01/prog08_print_how_many_odd_numbers_in_ten_input_numbers.py
@@ -1,6 +1,8 @@
 #Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 
 #Ask the user for 10 input numbers
+for i in range(10):
+    num = int(input(f"Enter a number {i+1}: "))
 
 #Determine if odd or even
 
