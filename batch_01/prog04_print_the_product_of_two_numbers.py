aa398fdac79e5db4312f5b4571a267197ed06429
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:35:41 +0800

    Prog04: Ask User for 2 input numbers

--- batch_01/prog04_print_the_product_of_two_numbers.py
+++ batch_01/prog04_print_the_product_of_two_numbers.py
@@ -1,6 +1,9 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 #Ask the user for 2 input numbers
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #Get the product
 
