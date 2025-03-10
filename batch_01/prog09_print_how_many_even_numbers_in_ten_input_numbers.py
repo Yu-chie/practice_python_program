64b66b387656b6d956146142803dd54cf3c6e0c7
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 19:05:39 +0800

    Prog09: Pseudocode Added

---
+++ batch_01/prog09_print_how_many_even_numbers_in_ten_input_numbers.py
@@ -0,0 +1,7 @@
+#Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+ 
+#Define the range, Loop through numbers 0 to 100
+ 
+#Check if number is even
+ 
+#Print all even numbers
