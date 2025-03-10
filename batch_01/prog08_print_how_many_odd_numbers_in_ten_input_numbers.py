38af0b8b386ab56cb18593ab2cea1c4140771e2b
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 18:58:19 +0800

    Prog08: Pseudocode Added

---
+++ batch_01/prog08_print_how_many_odd_numbers_in_ten_input_numbers.py
@@ -0,0 +1,7 @@
+#Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+#Ask the user for 10 input numbers
+
+#Determine if odd or even
+
+#Print the number of odd numbers
