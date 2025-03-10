45f304e68f58263658ab255ce7b285a0b325268a
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 18:27:52 +0800

    Prog07: Fix: Sum initialized

--- batch_01/prog07_print_the_sum_of_ten_numbers.py
+++ batch_01/prog07_print_the_sum_of_ten_numbers.py
@@ -1,9 +1,12 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
+#Initialize sum
+sum = 0
+
 #Ask the user for 10 input numbers
 for i in range(10):
     num = int(input(f"Enter a number {i+1}: "))
-    sum = 0
+
     #Get the sum
     sum += num
 
