b62d00ec2444faa714bdd54f5ff6b349a2f63e2a
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-10 17:29:42 +0800

    Prog03: Ask User for 2 input numbers

--- batch_01/prog03_print_the_sum_of_the_two_numbers.py
+++ batch_01/prog03_print_the_sum_of_the_two_numbers.py
@@ -1,6 +1,9 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 #Ask the user for 2 input numbers
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #Get the sum
 
