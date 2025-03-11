31fae5117251d717f5544856665d9f9e58d0a773
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:42:55 +0800

    Prog10: Pseudocode Added

---
+++ batch_02/prog10_print_all_numbers_between_two_numbers.py
@@ -0,0 +1,9 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+#Ask user to input 2 numbers
+
+#Determine smaller and bigger number
+
+#Find the numbers between the two
+
+#Print numbers
