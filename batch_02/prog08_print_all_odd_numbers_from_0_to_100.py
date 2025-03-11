4722cbb473424b0ebd64b46c451c5f86a7fc6434
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:23:14 +0800

    Prog08: Pseudocode Added

---
+++ batch_02/prog08_print_all_odd_numbers_from_0_to_100.py
@@ -0,0 +1,9 @@
+#Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+#Initialize num
+
+#Loop 0 to 100
+
+#Check if number is odd
+
+#Print odd numbers
