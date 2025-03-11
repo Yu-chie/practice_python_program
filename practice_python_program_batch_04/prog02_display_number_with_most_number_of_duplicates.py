0ba7b7512d345c7ab8af77f484015de10855603d
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 07:04:58 +0800

    Prog02: Initialize dictionary for number frequencies, Ask for User input

--- practice_python_program_batch_04/prog02_display_number_with_most_number_of_duplicates.py
+++ practice_python_program_batch_04/prog02_display_number_with_most_number_of_duplicates.py
@@ -1,9 +1,13 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 #Initialize dictionary to strore number frequencies
+number_counts = {}
 
 #Add loop to take number input untill invalid
-
+while True:
+    num = input("Enter a number: ")
+    
+    
     #Add number to dictionary and update count
 
 #Find the number with most duplicates
