68676d69c7799348e70d195a3ae352184a90afab
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 13:59:42 +0800

    Prog01: Added dictionary for occurences of numbers

--- batch_03/prog01_display_unique_numbers.py
+++ batch_03/prog01_display_unique_numbers.py
@@ -1,5 +1,8 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
+#Dictionary to count occurences of numbers
+count_numb = {}
+
 #Ask user for 10 input numbers
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
