07235797cc4934930311cc69b395bfb05b210f64
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 19:03:07 +0800

    Prog04: Pseudocode Added

---
+++ batch_03/prog05_sort_user_input_numbers.py
@@ -0,0 +1,13 @@
+#Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+#Initialize list for numbers
+
+#Start loop to continuously ask for user
+
+#Try to convert input to integer
+
+#Append valid number
+
+#Sort numbers in ascending orger
+
+#Print sorted numbers
