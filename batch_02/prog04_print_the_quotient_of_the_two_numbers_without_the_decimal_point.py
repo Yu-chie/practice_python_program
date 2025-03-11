ee1c0b8edd3608a25392fe75f236960fc23b63d0
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 10:51:23 +0800

    Prog04: Pseudocode Added

---
+++ batch_02/prog04_print_the_quotient_of_the_two_numbers_without_the_decimal_point.py
@@ -0,0 +1,7 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+#Ask user for 2 numbers
+
+#Divide the numbers
+
+#Print quotient without decimal point
