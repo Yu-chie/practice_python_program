11ab82f4014e90549a4f27aa397f18595586f1e8
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 06:32:47 +0800

    Prog01: Initialize list to store user input, Ask input from user

--- prog01_display_all_numbers_with_duplicates.py
+++ prog01_display_all_numbers_with_duplicates.py
@@ -1,7 +1,11 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 #Ask User for 10 input numbers
-
+numbers = []
+for i in range(10):
+    num = int(input("Enter a number: "))
+    numbers.append(num)
+    
 #Initialize set to track seen and duplicate numbers
 
 #Check for duplicates
