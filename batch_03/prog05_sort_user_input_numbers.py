8d43302249bedae455bff3fb55d65bcf15d5163b
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 19:06:44 +0800

    Prog05: Initialize list and start user input loop

--- batch_03/prog05_sort_user_input_numbers.py
+++ batch_03/prog05_sort_user_input_numbers.py
@@ -1,8 +1,11 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 
 #Initialize list for numbers
+numbers = []
 
 #Start loop to continuously ask for user
+while True:
+    num = input("Enter a number: ")
 
 #Try to convert input to integer
 
