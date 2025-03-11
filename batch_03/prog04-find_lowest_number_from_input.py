155d6a45d27be980df47f4970b1afaff3ecb3fb3
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 18:50:01 +0800

    Prog04: Add loop to continuously ask for number input

--- batch_03/prog04-find_lowest_number_from_input.py
+++ batch_03/prog04-find_lowest_number_from_input.py
@@ -1,8 +1,11 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
 #Initialize variable for lowest number
+lowest_number = None
 
 #Start a loop to continuously ask for input
+while True:
+    num = input("Enter a number: ")
 
 #Try to convert input to an integer
 #If input is invalid, exit loop
