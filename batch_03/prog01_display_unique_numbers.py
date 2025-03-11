55f5751263bc609d9a5319903f70ca5f0fa57c51
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 12:03:51 +0800

    Prog01: Pseudocode Added

---
+++ batch_03/prog01_display_unique_numbers.py
@@ -0,0 +1,9 @@
+#Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+#Ask user for 10 input numbers
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+
+#Check if they have duplicate
+
+#Print unique numbers
