d1b49986cb609a38d5b37037565bec42cfee47ac
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 10:59:53 +0800

    Prog05: Ask User for 2 input numbers

--- batch_02/prog05_print_the_remainder_when_the_first_number_is_divided_by_the_second_number.py
+++ batch_02/prog05_print_the_remainder_when_the_first_number_is_divided_by_the_second_number.py
@@ -1,6 +1,9 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 
 #Ask user for 2 numbers
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #Divide num1 by num2
 
