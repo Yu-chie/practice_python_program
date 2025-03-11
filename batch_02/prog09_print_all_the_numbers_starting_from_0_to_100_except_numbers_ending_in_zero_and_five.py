ad5cab2c1f8d13e21676092185da967686bec823
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:33:17 +0800

    Prog09: Pseudocode Added

---
+++ batch_02/prog09_print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_and_five.py
@@ -0,0 +1,7 @@
+#Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+#Loop 0 to 100
+
+#Check if number end with 0 or 5
+
+#Print all numbers
