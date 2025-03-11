b4c1220abd834e03483adc6e2776976f0fd7117d
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 14:18:03 +0800

    Prog02: Pseudocode Added

---
+++ batch_03/prog02_display_first_occurence.py
@@ -0,0 +1,13 @@
+#Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+
+#Initialize list to store input numbers
+
+#Ask User for 10 numbers
+
+#Check if number is already in the list
+
+    #If not in the list, print then add to the list
+
+    #If already in list, ignore
+
+#Print all numbers without duplicate
