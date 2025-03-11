b5c19b9f19b4b559a85e08e96cc2a6f43fd05aca
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:16:16 +0800

    Prog07: Pseudocode Added

---
+++ batch_02/prog07_print_how_many_are_even_numbers_in_ten_input_numbers.py
@@ -0,0 +1,9 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+#Initialize count
+
+#Ask user for 10 numbers
+
+#Check even numbers
+
+#Print the number of even numbers
