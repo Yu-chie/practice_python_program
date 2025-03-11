b8163a3d1cc58e2e3f8768d39cc0fa3af167b363
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:25:52 +0800

    Prog08: Initialize num, Loop 0 to 100

--- batch_02/prog08_print_all_odd_numbers_from_0_to_100.py
+++ batch_02/prog08_print_all_odd_numbers_from_0_to_100.py
@@ -1,9 +1,12 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
 #Initialize num
+num = 0
 
 #Loop 0 to 100
+while num < 100:
 
-#Check if number is odd
-
+    #Check if number is odd
+    print()
+    
 #Print odd numbers
