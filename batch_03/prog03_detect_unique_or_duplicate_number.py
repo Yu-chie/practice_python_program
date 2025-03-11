1e2024e93390f0a9f59e4a35ace0cfee422dfd7d
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 14:54:54 +0800

    Prog03: Pseudocode Added

---
+++ batch_03/prog03_detect_unique_or_duplicate_number.py
@@ -0,0 +1,17 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+#Initialize a set to track numbers
+
+#Start a loop to continuously ask for input
+
+    #Try to get an integer input from the user
+
+    #If input is invalid, break loop
+
+#Check if the number has been entered before
+
+    #If its in set print Duplicate
+
+    #If its not, print Unique
+
+#End the program when an invalid input is given
