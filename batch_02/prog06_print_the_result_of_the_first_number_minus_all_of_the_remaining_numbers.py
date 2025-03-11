af8e9545f72ea76310f973278614568f9a207ca8
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:09:25 +0800

    Prog06: Ask User for 10 input numbers

--- batch_02/prog06_print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
+++ batch_02/prog06_print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -1,6 +1,10 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 #Ask user for 10 numbers
+print("Please Enter a Number")
+num1 = int(input("Enter number 1: "))
+for i in range(9):
+    num = int(input(f"Enter number {i+2}: "))
 
 #Subtract 1st number with the remaining number
 
