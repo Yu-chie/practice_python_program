596795ea2aedafd31d9405c90e530c251b2e518a
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:06:27 +0800

    Prog06: Pseudocode Added

---
+++ batch_02/prog06_print_the_result_of_the_first_number_minus_all_of_the_remaining_numbers.py
@@ -0,0 +1,7 @@
+#Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+#Ask user for 10 numbers
+
+#Subtract 1st number with the remaining number
+
+#Print the result
