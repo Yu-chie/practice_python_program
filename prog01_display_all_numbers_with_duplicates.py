63e60c583f5d956445c018cdd2498fa830546bcb
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 06:30:21 +0800

    Prog01: Pseudocode Added

---
+++ prog01_display_all_numbers_with_duplicates.py
@@ -0,0 +1,9 @@
+#Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+#Ask User for 10 input numbers
+
+#Initialize set to track seen and duplicate numbers
+
+#Check for duplicates
+
+#Print Duplicate numbers
