f3d44777484b3cfd88249360827cf213806a49fd
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 14:57:47 +0800

    Prog03: Add input loop to continuously ask for numbers

--- batch_03/prog03_detect_unique_or_duplicate_number.py
+++ batch_03/prog03_detect_unique_or_duplicate_number.py
@@ -1,8 +1,11 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
 #Initialize a set to track numbers
+seen_numbers = set()
 
 #Start a loop to continuously ask for input
+while True:
+    num = input("Enter a number: ")
 
     #Try to get an integer input from the user
 
