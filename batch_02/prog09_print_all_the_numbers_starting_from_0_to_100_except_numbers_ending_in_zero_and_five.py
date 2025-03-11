6c3656ed23c16dc42ca998537e96c6d83957fa1f
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:35:31 +0800

    Prog09: Loop 0 to 100, Added if function

--- batch_02/prog09_print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_and_five.py
+++ batch_02/prog09_print_all_the_numbers_starting_from_0_to_100_except_numbers_ending_in_zero_and_five.py
@@ -1,7 +1,9 @@
 #Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
 #Loop 0 to 100
+for num in range(101):
 
-#Check if number end with 0 or 5
-
+    #Check if number end with 0 or 5
+    if num:
+        print()
 #Print all numbers
