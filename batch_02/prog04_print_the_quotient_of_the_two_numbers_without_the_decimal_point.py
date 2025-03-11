089663e04c23a4a9c8b2dacc006bb1de93ca01b6
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 10:52:21 +0800

    Prog04: Ask User for 2 input numbers

--- batch_02/prog04_print_the_quotient_of_the_two_numbers_without_the_decimal_point.py
+++ batch_02/prog04_print_the_quotient_of_the_two_numbers_without_the_decimal_point.py
@@ -1,6 +1,9 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
 #Ask user for 2 numbers
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #Divide the numbers
 
