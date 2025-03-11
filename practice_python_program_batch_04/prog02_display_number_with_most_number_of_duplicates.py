48e6782d795e4ae00373c84c7923b077dd6eadfd
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 06:55:30 +0800

    Prog02: Pseudocode Added

---
+++ practice_python_program_batch_04/prog02_display_number_with_most_number_of_duplicates.py
@@ -0,0 +1,11 @@
+#Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+#Initialize dictionary to strore number frequencies
+
+#Add loop to take number input untill invalid
+
+    #Add number to dictionary and update count
+
+#Find the number with most duplicates
+
+#Print the number with most duplicates
