24accea248d353333b017546aa9df1f6f00febea
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:17:36 +0800

    Prog07: Initialize count, Ask User for 10 input numbers

--- batch_02/prog07_print_how_many_are_even_numbers_in_ten_input_numbers.py
+++ batch_02/prog07_print_how_many_are_even_numbers_in_ten_input_numbers.py
@@ -1,8 +1,11 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
 #Initialize count
+count = 0
 
 #Ask user for 10 numbers
+for i in range(10):
+    num = int(input(f"Enter a number {i+1}: "))
 
 #Check even numbers
 
