17fa22a2c2bbef851ad5c729df7bbc101e1eadca
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 10:58:30 +0800

    Prog05: Pseudocode Added

---
+++ batch_02/prog05_print_the_remainder_when_the_first_number_is_divided_by_the_second_number.py
@@ -0,0 +1,7 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+#Ask user for 2 numbers
+
+#Divide num1 by num2
+
+#Print the remainder
