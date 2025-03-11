987eefe257a04fc08e9f3e97fd4ff944ddcd60c9
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 18:47:04 +0800

    Prog04: Pseudocode Added

---
+++ batch_03/prog04-find_lowest_number_from_input.py
@@ -0,0 +1,12 @@
+#Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+#Initialize variable for lowest number
+
+#Start a loop to continuously ask for input
+
+#Try to convert input to an integer
+#If input is invalid, exit loop
+
+#Check if its the lowest number so far
+
+#Print the lowest number
