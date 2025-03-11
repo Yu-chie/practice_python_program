6f6d6d1a0a9aa95ab55b9be8f47fb835d02f5159
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 14:20:53 +0800

    Prog02: Initialize list for numbers, Ask User for 10 input numbers

--- batch_03/prog02_display_first_occurence.py
+++ batch_03/prog02_display_first_occurence.py
@@ -1,8 +1,12 @@
 #Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 #Initialize list to store input numbers
+numbers = []
 
 #Ask User for 10 numbers
+for i in range(10):
+    num = int(input(f"Enter a number {i+1}: "))
+    numbers.append(num)
 
 #Check if number is already in the list
 
