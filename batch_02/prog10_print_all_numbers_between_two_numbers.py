54259e45fdf7858920ea2ebe9c06ed904cf00d28
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-11 11:44:02 +0800

    Prog10: Ask User for 2 input numbers

--- batch_02/prog10_print_all_numbers_between_two_numbers.py
+++ batch_02/prog10_print_all_numbers_between_two_numbers.py
@@ -1,6 +1,9 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 #Ask user to input 2 numbers
+print("Please Enter a number")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 #Determine smaller and bigger number
 
