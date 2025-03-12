c474e30677a8afcbc0aad651c96f0c6c03925e47
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 21:09:47 +0800

    Prog03: Initialize variable to track highest number

--- practice_python_program_batch_04/prog03_find_highest_number.py
+++ practice_python_program_batch_04/prog03_find_highest_number.py
@@ -1,6 +1,7 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
 #Initialize variable to track highest number
+highest_number = None
 
 #Add loop to take continuous number inut until invalid
 
