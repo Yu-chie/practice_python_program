5e25680ac7c7165e1ba1262f29b6707de8dee58f
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 21:50:17 +0800

    Prog05: Pseudocode Added

---
+++ practice_python_program_batch_04/prog05_display_average_of_numbers.py
@@ -0,0 +1,11 @@
+#Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+#Initialize list to store input numbers
+
+#Add loop to take continuous number input until invalid
+
+    #Try converting input to integer
+
+#Compute the Average
+
+#Print the Average
