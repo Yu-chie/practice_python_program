09b9997cb7350f35c5125df406800c4a5cd322d2
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 21:52:43 +0800

    Prog05: Add loop to take continuous number input until invalid

--- practice_python_program_batch_04/prog05_display_average_of_numbers.py
+++ practice_python_program_batch_04/prog05_display_average_of_numbers.py
@@ -4,8 +4,14 @@
 numbers = []
 
 #Add loop to take continuous number input until invalid
+while True:
+    num = input("Enter a number: ")
 
     #Try converting input to integer
+    try:
+        num = int(num)
+    except ValueError:
+        break
 
 #Compute the Average
 
