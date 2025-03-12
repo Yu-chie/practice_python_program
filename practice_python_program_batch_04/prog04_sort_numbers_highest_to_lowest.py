2d4371926e2c85a30ab300d8ae0d42ee81b7fb7d
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 21:42:36 +0800

    Prog04: Add loop to take continuous number input until valid, Append valid numbers to list

--- practice_python_program_batch_04/prog04_sort_numbers_highest_to_lowest.py
+++ practice_python_program_batch_04/prog04_sort_numbers_highest_to_lowest.py
@@ -1,13 +1,20 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
 #Initialize list to store input numbers
+numbers = []
 
 #Add loop to take continuous number input until invalid
+while True:
+    num = input("Enter a number: ")
 
     #Try converting input to integer
+    try:
+        num = int(num)
+    except ValueError:
+        break
 
     #Append valid number to list
-
+    numbers.append(num)
 #Sort numbers in descending order
 
 #Print sorted numbers
