a6f594b43eefbcef3a791d94de5d600c0ff0dc42
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 21:51:10 +0800

    Prog05: Initialize list to store input numbers

--- practice_python_program_batch_04/prog05_display_average_of_numbers.py
+++ practice_python_program_batch_04/prog05_display_average_of_numbers.py
@@ -1,6 +1,7 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
 #Initialize list to store input numbers
+numbers = []
 
 #Add loop to take continuous number input until invalid
 
