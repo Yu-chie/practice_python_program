b9dc3be0b34138721c30a3211bb5a8ca7f547fd0
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 21:40:27 +0800

    Prog04: Pseudocode Added

---
+++ practice_python_program_batch_04/prog04_sort_numbers_highest_to_lowest.py
@@ -0,0 +1,13 @@
+#Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+#Initialize list to store input numbers
+
+#Add loop to take continuous number input until invalid
+
+    #Try converting input to integer
+
+    #Append valid number to list
+
+#Sort numbers in descending order
+
+#Print sorted numbers
