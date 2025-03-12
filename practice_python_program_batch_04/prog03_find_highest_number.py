5881e52c4f8ee2480b30602bbd83fa66b211f8a4
Victoria Yuki M. Ilano <rrainy011306@gmail.com>
2025-03-12 21:04:35 +0800

    Prog03: Pseudocode Added

---
+++ practice_python_program_batch_04/prog03_find_highest_number.py
@@ -0,0 +1,11 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+#Initialize variable to track highest number
+
+#Add loop to take continuous number inut until invalid
+
+    #Try converting input to integer
+
+#Update highest number if larger number is found
+
+#Print the highest number
